a16c949bbec06e9d2af6ab362038d9b227b0801d
Nivall <38981440+k-kotov@users.noreply.github.com>
2020-04-09 15:45:07 +0300

    Update variables.tf

--- variables.tf
+++ variables.tf
@@ -6,6 +6,7 @@
 variable "gcp_project_id" {
   description = "The name of the GCP Project where all resources will be launched."
   type        = string
+  default = "000"
 }
 
 variable "gcp_region" {
